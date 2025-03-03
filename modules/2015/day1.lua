9b60609e0bb6242ec67ee70fcdfa21c348dbee48
nhlmg93 <nhelmig93@gmail.com>
2025-03-03 09:47:12 -0600

    day1 part2 done

--- modules/2015/day1.lua
+++ modules/2015/day1.lua
@@ -1,6 +1,6 @@
 M = {}
 
-function M:partOne(instructions)
+function M.partOne(instructions)
   local floor = 0
 
   for i = 1, #instructions do
@@ -13,6 +13,26 @@ function M:partOne(instructions)
   end
 
   return floor
+end
+
+function M.partTwo(instructions)
+  local floor = 0
+
+  for i = 1, #instructions do
+    local char = instructions:sub(i, i)
+    if char == "(" then
+      floor = floor + 1
+    elseif char == ")" then
+      floor = floor - 1
+    end
+
+    if floor == -1 then
+      return i
+    end
+  end
+
+  return nil
+
 end
 
 return M
--- tests/2015/test_day1.lua
+++ tests/2015/test_day1.lua
@@ -5,9 +5,14 @@ local day1 = require("modules.2015.day1")
 TestDay1 = {}
 
 function TestDay1:testPartOne()
-  local input = utils.readFileStripWhitespace("assets/day1_p1.txt")
-  local floors = day1:partOne(input)
+  local input = utils.readFileStripWhitespace("assets/day1.txt")
+  local floors = day1.partOne(input)
   luaunit.assertEquals(floors, 138)
+end
+function TestDay1:testPartTwo()
+  local input = utils.readFileStripWhitespace("assets/day1.txt")
+  local floors = day1.partTwo(input)
+  luaunit.assertEquals(floors, 1771)
 end
 
 return TestDay1
